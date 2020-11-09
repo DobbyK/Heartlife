90e93a6b77043524a794054e200e23be032e125a
DobbyK <69256884+DobbyK@users.noreply.github.com>
2020-11-09 07:13:16 -0700

    Formatting Update - Main.py

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
+# Import Everything
 import functions
 import references.config
 import cmline.cdline
 import pygame
 from replit import db
 
+# Default Settings
 pygame.init()
-
+# Opening Stuff 
 functions.cutscenes.openingmenu()
 
 if references.config.newbie == True:
   functions.cutscenes.newbie_adventure()
 elif references.config.newbie == False:
   functions.spawn.newgame()
-		
+
+# Command line loop
 while references.config.free == True:
 	chc = input('>> ')
 	db["cd"] = chc
